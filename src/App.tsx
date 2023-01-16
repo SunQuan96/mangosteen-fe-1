270f7661d73b4b9ca6130ed4290a3f9ba848d07e
SunQuan96 <1097610301sq@gmail.com>
2023-01-17 04:21:29 +0800

    Overlay组件

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,9 @@
-import { defineComponent} from 'vue';
-import {RouterView }from 'vue-router';
+import { defineComponent, Transition, VNode } from "vue";
+import { RouteLocationNormalizedLoaded, RouterView } from "vue-router";
 import "./App.scss"
-export const App =defineComponent({
-setup() {
-return() => 
-(<div class="page"><RouterView/></div>)
-}
+export const App = defineComponent({
+    setup() {
+        return () =>
+            (<div class="page"><RouterView /></div>)
+    }
 })
