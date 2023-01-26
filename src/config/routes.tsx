59ee8ed5995410e39320bb73711c2dd590378f41
SunQuan96 <1097610301sq@gmail.com>
2023-01-27 00:38:54 +0800

    添加组件

--- src/config/routes.tsx
+++ src/config/routes.tsx
@@ -11,17 +11,17 @@ import { StartPage } from "../views/StartPage";
 import { Welcome } from "../views/Welcome";
 
 export const routes: RouteRecordRaw[] = [
-    { path: '/', redirect: '/welcome' },
-    {
-        path: '/welcome',
-        component: Welcome,
-        children: [
-            { path: '', redirect: '/welcome/1' },
-            { path: '1', name: "Welcome1", components: { main: First, footer: FirstActions }, },
-            { path: '2', name: "Welcome2", components: { main: Second, footer: SecondActions }, },
-            { path: '3', name: "Welcome3", components: { main: Third, footer: ThirdActions }, },
-            { path: '4', name: "Welcome4", components: { main: Forth, footer: ForthActions }, },
-        ]
-    },
-    { path: '/start', component: StartPage }
+  { path: '/', redirect: '/welcome' },
+  {
+    path: '/welcome',
+    component: Welcome,
+    children: [
+      { path: '', redirect: '/welcome/1' },
+      { path: '1', name: "Welcome1", components: { main: First, footer: FirstActions }, },
+      { path: '2', name: "Welcome2", components: { main: Second, footer: SecondActions }, },
+      { path: '3', name: "Welcome3", components: { main: Third, footer: ThirdActions }, },
+      { path: '4', name: "Welcome4", components: { main: Forth, footer: ForthActions }, },
+    ]
+  },
+  {path:'/start', component: StartPage}
 ]
--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,11 @@
-import {routes} from './config/routes'
+import { routes } from './config/routes';
 import { createApp } from 'vue'
-import{ App } from './App'
-import  { createRouter }from 'vue-router'
-import { history }from './shared/history'
+import { App } from './App'
+import { createRouter } from 'vue-router'
+import { history } from './shared/history';
+import '@svgstore';
 
-const router = createRouter({ history,routes })
+const router = createRouter({ history, routes })
 
 const app = createApp(App)
 app.use(router)
